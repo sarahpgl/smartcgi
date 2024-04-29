c6aae45fdbcebd1312cf63b3b4ae803ac560a093
ThibautChantrel <thibaut.chantrel0102@gmail.com>
2024-04-29 17:25:35 +0200

    [Feat] - Ajout des liens entre les pages depuis le menu

--- workspaces/front/src/js/components/MenuComponent/MenuComponent.tsx
+++ workspaces/front/src/js/components/MenuComponent/MenuComponent.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 
 import styles from './MenuComponent.module.css';
-
-
+import { useNavigate } from "react-router-dom";
 
 const MenuComponent = () => {
+    const navigate = useNavigate(); // Utilisation de useNavigate pour la navigation
+    
+    // Fonction pour rediriger vers la page associée
+    const redirectToPage = (path) => {
+        navigate(path);
+    };
+
     return (
         <div className={styles.container}>
-            <button className={styles.button}>Créer une partie</button>
-            <button className={styles.button}>Rejoindre une partie</button>
-            <button className={styles.button}>Carnet Green IT</button>
-            <button className={styles.button}>Règles du jeu</button>
-            <button className={styles.button}>Visualiser les cartes</button>
-            
+            {/* Boutons avec des fonctions onClick pour rediriger vers les pages associées */}
+            <button className={styles.button} onClick={() => redirectToPage('/createGame')}>Créer une partie</button>
+            <button className={styles.button} onClick={() => redirectToPage('/joinGame')}>Rejoindre une partie</button>
+            <button className={styles.button} onClick={() => redirectToPage('/greenIt')}>Carnet Green IT</button>
+            <button className={styles.button} onClick={() => redirectToPage('/rules')}>Règles du jeu</button>
+            <button className={styles.button} onClick={() => redirectToPage('/viewCards')}>Visualiser les cartes</button>
         </div>
     );
 };
-
 export default MenuComponent;
