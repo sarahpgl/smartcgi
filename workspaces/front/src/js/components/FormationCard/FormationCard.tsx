fb786d338a096c22b02b7698c30374a83e468859
Gregoire <64920340+greg315315@users.noreply.github.com>
2024-04-30 15:50:43 +0200

    [Feat] Composant FormationCard

---
+++ workspaces/front/src/js/components/FormationCard/FormationCard.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+import styles from './FormationCard.module.css';
+
+import iconFormationDev from '../../images/Formation_dev.webp';
+import iconFormationLeadTech from '../../images/Formation_lead_tech.webp';
+import iconFormationProductOwner from '../../images/Formation_product_owner.webp';
+
+import actorIcon from '@app/icons/actor_icon.webp';
+
+import { Formation_Card } from '@shared/common/Cards';
+
+function FormationCard(card:Formation_Card) {
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardheader}>
+                <div className={styles.icon}>
+                    <img src={card.actor === 'Developer' ? iconFormationDev : card.actor === 'Architect' ? iconFormationLeadTech : card.actor === 'ProductOwner' ? iconFormationProductOwner : iconFormationProductOwner} alt="iconFormation" />
+                </div>
+                <div>
+                <h2>Formation</h2>
+                <h3>{card.actor==="Developer" ? "développement green" : card.actor==="Architect" ? "écoconception tech" : card.actor==="ProductOwner" ? "frugalité fonctionnelle" : "type inconnu"}</h3>
+                </div>
+            </div>
+
+
+            <div className={styles.description}>
+                <div className={styles.descriptionheader}>
+                    <h3>{card.title ? card.title : "Sample title"}</h3>
+                </div>
+                <div className={styles.descriptionbody}>
+                    <p>{card.contents? card.contents : "blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla "}</p>
+                </div>
+            </div>
+
+        </div>
+
+    );
+}
+
+export default FormationCard;
--- workspaces/front/src/js/pages/home/home.tsx
+++ workspaces/front/src/js/pages/home/home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "@app/js/components/header/Header";
 import BestPracticeCard from "@app/js/components/BestPracticeCard/BestPracticeCard";
 import BadPracticeCard from '@app/js/components/BadPracticeCard/BadPracticeCard';
+import FormationCard from '@app/js/components/FormationCard/FormationCard';
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Header />
       <BestPracticeCard />
       <BadPracticeCard />
+      <FormationCard />
     </div>
   );
 }
