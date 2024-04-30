92ad4a763ab93c14893858a7273377b0c6386220
Gregoire <64920340+greg315315@users.noreply.github.com>
2024-04-30 16:13:29 +0200

    [feat] Composant ExpertCard

--- workspaces/front/src/js/pages/home/home.tsx
+++ workspaces/front/src/js/pages/home/home.tsx
@@ -3,6 +3,7 @@ import Header from "@app/js/components/header/Header";
 import BestPracticeCard from "@app/js/components/BestPracticeCard/BestPracticeCard";
 import BadPracticeCard from '@app/js/components/BadPracticeCard/BadPracticeCard';
 import FormationCard from '@app/js/components/FormationCard/FormationCard';
+import ExpertCard from '@app/js/components/ExpertCard/ExpertCard';
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <BestPracticeCard />
       <BadPracticeCard />
       <FormationCard />
+      <ExpertCard />
     </div>
   );
 }
