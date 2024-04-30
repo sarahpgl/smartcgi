51fff0e7a19ebfee9bf6b0d6218cfaefe7577d14
JadeLRX <jade.lrx0@gmail.com>
2024-04-30 17:38:43 +0200

    test deck card

--- workspaces/api/src/card/card.service.ts
+++ workspaces/api/src/card/card.service.ts
@@ -2,41 +2,43 @@ import { Injectable } from "@nestjs/common";
 import { parse } from "papaparse";
 import { CsvCard } from "./card.type";
 import { Card as EntityCard } from "@app/entity/card";
-import { Expert_Card } from "@app/entity/expert_card";
+import { Expert_Card as EntityExpert } from "@app/entity/expert_card";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Entity, Repository } from "typeorm";
-import { Best_Practice_Card } from "@app/entity/best_practice_card";
-import { Bad_Practice_Card } from "@app/entity/bad_practice_card";
-import { Training_Card } from "@app/entity/training_card";
+import { Best_Practice_Card as EntityBestPractice} from "@app/entity/best_practice_card";
+import { Bad_Practice_Card as EntityBadPractice } from "@app/entity/bad_practice_card";
+import { Training_Card as EntityTraining} from "@app/entity/training_card";
 import { Card_Content } from "@app/entity/card_content";
-import { Actor } from "@app/entity/actor";
+import { Actor as EntityActor} from "@app/entity/actor";
 import { Practice_Card } from "@app/entity/practice_card";
-import { Card } from "@shared/common/Cards";
+import { Card, Formation_Card, Best_Practice_Card, Bad_Practice_Card, Expert_Card } from "@shared/common/Cards";
 import { Best_Practices_Status } from "@app/entity/user_game";
-import { info } from "console";
+import { Actor } from "@shared/common/Cards";
+
 
 @Injectable()
 export class CardService {
   constructor(
     @InjectRepository(EntityCard)
     private cards_repository: Repository<EntityCard>,
-    @InjectRepository(Best_Practice_Card)
-    private best_practice_cards_repository: Repository<Best_Practice_Card>,
-    @InjectRepository(Bad_Practice_Card)
-    private bad_practice_cards_repository: Repository<Bad_Practice_Card>,
-    @InjectRepository(Expert_Card)
-    private expert_cards_repository: Repository<Expert_Card>,
-    @InjectRepository(Training_Card)
-    private training_cards_repository: Repository<Training_Card>,
+    @InjectRepository(EntityBestPractice)
+    private best_practice_cards_repository: Repository<EntityBestPractice>,
+    @InjectRepository(EntityBadPractice)
+    private bad_practice_cards_repository: Repository<EntityBadPractice>,
+    @InjectRepository(EntityExpert)
+    private expert_cards_repository: Repository<EntityExpert>,
+    @InjectRepository(EntityTraining)
+    private training_cards_repository: Repository<EntityTraining>,
     @InjectRepository(Card_Content)
     private card_contents_repository: Repository<Card_Content>,
-    @InjectRepository(Actor)
-    private actors_repository: Repository<Actor>,
+    @InjectRepository(EntityActor)
+    private actors_repository: Repository<EntityActor>,
   ) {}
 
+  // Method to parse CSV file and create cards
   async parseCsv(file: Express.Multer.File) {
-    console.log('on parse le csv');
     const csvData: CsvCard[] = [];
+    // Parsing CSV file
     const stream = parse(file.buffer.toString(), {
       header: true,
       complete: results => {
@@ -46,9 +48,10 @@ export class CardService {
     });
 
     const cards = [];
-    for (const row of csvData) {
+    // Iterating through parsed CSV data
+    for(const row of csvData){
+      // Extracting data from each row
       const { id, cardType, language, label, description, link, actorType, networkGain, memoryGain, cpuGain, storageGain, difficulty } = row;
-      console.log(row);
       let card: EntityCard = await this.cards_repository.findOne({ where: { id } });
       let card_already_exists = true;
       if (card == null) {
@@ -130,14 +133,101 @@ export class CardService {
     return cards;
   }
 
-  async getDeck(): Promise<Card[]> {
-    /* A faire récupérer les cartes avec leurs différents types et informations
-     *  Mélanger le deck et le renvoyer
-     */
-    return Promise.resolve([]);
+  
+  // Method to retrieve shuffled deck of cards
+  async getDeck(): Promise<any> {
+
+    // Shuffling and formatting bad practice cards
+    const badPracticeCards = await this.bad_practice_cards_repository.find({relations: ["contents", "actors"]});
+    console.log(badPracticeCards);
+    console.log(badPracticeCards[0].contents);
+    const deckBadPracticeCards = this.shuffleArray(badPracticeCards).slice(0, 12);
+    const formattedBadPracticeCards: Bad_Practice_Card[] = deckBadPracticeCards.map((card: EntityBadPractice
+    ) => ({
+      id: card.id.toString(),
+      actor: this.getActorName(card.actors[0].title), 
+      title: card.contents[0] ? card.contents[0].label : 'No label', 
+      contents: card.contents[0] ? card.contents[0].description : 'No description', 
+      cardType: 'BadPractice',
+      network_gain: card.network_gain,
+      memory_gain: card.memory_gain,
+      cpu_gain: card.cpu_gain,
+      storage_gain: card.storage_gain,
+      difficulty: card.difficulty,
+    }));
+    
+    // Shuffling and formatting best practice cards
+    const bestPracticeCards = await this.best_practice_cards_repository.find({relations: ["contents", "actors"]});
+    const deckBestPracticeCards = this.shuffleArray(bestPracticeCards).slice(0, 50);
+    const formattedBestPracticeCards: Best_Practice_Card[] = deckBestPracticeCards.map((card: EntityBestPractice) => ({
+      id: card.id.toString(),
+      actor: this.getActorName(card.actors[0].title), 
+      title: card.contents[0] ? card.contents[0].label : 'No label', 
+      contents: card.contents[0] ? card.contents[0].description : 'No description',
+      cardType: 'BestPractice',
+      network_gain: card.network_gain,
+      memory_gain: card.memory_gain,
+      cpu_gain: card.cpu_gain,
+      storage_gain: card.storage_gain,
+      difficulty: card.difficulty,
+      carbon_loss: card.carbon_loss,
+    }));
+
+    // Shuffling and formatting expert cards
+    const expertCards = await this.expert_cards_repository.find({relations: ["contents", "actors"]});
+    const deckExpertCards = this.shuffleArray(expertCards).slice(0, 3);
+    const formattedExpertCards: Expert_Card[] = deckExpertCards.map((card: EntityExpert) => ({
+      id: card.id.toString(),
+      actor: this.getActorName(card.actors[0].title), 
+      title: card.contents[0] ? card.contents[0].label : 'No label', 
+      contents: card.contents[0] ? card.contents[0].description : 'No description', 
+      cardType: 'Expert',
+    }));
+
+    // Shuffling and formatting training cards
+    const trainingCards = await this.training_cards_repository.find({relations: ["contents", "actors"]});
+    const deckTrainingCards = this.shuffleArray(trainingCards).slice(0, 18);
+    const formattedTrainingCards: Formation_Card[] = deckTrainingCards.map((card: EntityTraining) => ({
+      id: card.id.toString(),
+      actor: this.getActorName(card.actors[0].title), 
+      title: card.contents[0] ? card.contents[0].label : 'No label', 
+      contents: card.contents[0] ? card.contents[0].description : 'No description', 
+      cardType: 'Formation',
+      linkToFormation: card.link, 
+    }));
+
+
+    const allCards = [formattedBadPracticeCards, formattedBestPracticeCards, formattedExpertCards, formattedTrainingCards];
+
+    const shuffledCards = this.shuffleArray(allCards);
+    console.log(shuffledCards); 
+    return shuffledCards;
+
+  }
+
+  private shuffleArray(array: any[]): any[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  private getActorName(actorTitle : string): Actor{
+    switch(actorTitle){
+      case 'Architecte':
+        return 'Architect';
+      case 'Développeur':
+        return 'Developer';
+      case 'Product Owner':
+        return 'ProductOwner';
+      default:
+        throw new Error(`Unexpected actor title: ${actorTitle}`);
+    }
   }
 
   async getBadPracticeCard(): Promise<Bad_Practice_Card[]> {
     return await this.bad_practice_cards_repository.find({ relations: ["contents"] });
   }
+
 }
