4df93ceee067827fcc4f4a795824e4d535648017
meiije <meije.pigeonnat@insa-lyon.fr>
2024-04-30 16:42:11 +0200

    OpponentBoard

---
+++ workspaces/front/src/js/components/OpponentBoard/OpponentBoard.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import expertPO from '../../images/Expert_product_owner.webp';
+import MPPO from '../../images/MP_product_owner.webp';
+import expertD from '../../images/Expert_dev.webp';
+import MPD from '../../images/MP_dev.webp';
+import expertLT from '../../images/Expert_lead_tech.webp';
+import MPLT from '../../images/MP_lead_tech.webp';
+import BestPracticeCard from "@app/js/components/BestPracticeCard/BestPracticeCard";
+import styles from './OpponentBoard.module.css';
+
+const OpponentBoard: React.FC = () => {
+
+    const data = {
+        name : "Marc",
+        kg: "900",
+        points: "3",
+        expert : "ProductOwner",
+        MP : "Developer"
+    };
+
+    let expertImageSrc = null;
+    if (data.expert === "Developer") {
+        expertImageSrc = expertD;
+    } else if (data.expert === "Architect") {
+        expertImageSrc = expertLT;
+    } else if (data.expert === "ProductOwner") {
+        expertImageSrc = expertPO;
+    }
+
+    let mpImageSrc =null;
+    if (data.MP === "Developer") {
+        mpImageSrc = MPD;
+    } else if (data.MP === "Architect") {
+        mpImageSrc = MPLT;
+    } else if (data.MP === "ProductOwner") {
+        mpImageSrc = MPPO;
+    }
+
+
+    return (
+        <>
+            <div className={styles.nameContainer}> </div>
+            <div className={styles.container}>
+            <label className={styles.labelname}>{data.name}</label>
+                <div className={styles.container2}>
+                    <label className={styles.label}>{data.kg}kg</label> <br />
+                    {expertImageSrc && <img src={expertImageSrc} alt="Carte expert" className={styles.cardImage} />} <br />
+                    {mpImageSrc && <img src={mpImageSrc} alt="Carte MP" className={styles.cardImage} />} <br />
+                    <label className={styles.labelPoints}>{data.points} points de sensibilisation</label> <br />
+                </div>
+                <BestPracticeCard /> {}
+            </div>
+        </>
+        
+    );
+};
+
+export default OpponentBoard;
---
+++ workspaces/front/src/js/components/SensibilisationQuizz/SensibilisationQuizz.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import styles from './SensibilisationQuizz.module.css';
+
+const Quizz: React.FC = () => {
+
+    const data = {
+        question: "Visionner un film en basse résolution plutôt qu’en haute définition, divise de combien la consommation d’énergie totale nécessaire ?",
+        answer1: "Entre 2 et 4 fois",
+        answer2: "Entre 4 et 10 fois",
+        answer3: "Entre 10 et 20 fois",
+        answerTrue: 2
+    };
+    const [answerCorrect, setanswerCorrect] = useState(false);
+
+    const [resultMessage, setResultMessage] = useState("");
+    const [answerChoisie, setanswerChoisie] = useState<number | null>(null);
+    const [quizzPlay, setQuizzPlay] = useState(false);
+    const [tempsRestant, setTempsRestant] = useState(10);
+    
+    useEffect(() => {
+        let timer: NodeJS.Timeout;
+
+        if (tempsRestant > 0) {
+            timer = setTimeout(() => {
+                setTempsRestant(tempsRestant - 1);
+            }, 1000);
+        } else if (!quizzPlay && tempsRestant === 0) {
+            setResultMessage("Temps écoulé !");
+            setQuizzPlay(true);
+        }
+
+        return () => clearTimeout(timer);
+    }, [quizzPlay, tempsRestant]);
+
+    const handleResult = (answerIndex: number) => {
+        if (!quizzPlay) {
+            if (answerIndex === data.answerTrue) {
+                setResultMessage(`Bien joué, vous avez gagné un point de sensibilisation !`);
+                setanswerCorrect(true);
+            } else {
+                setResultMessage(`Dommage, ce n’est pas la bonne réponse !`);
+            }
+            setanswerChoisie(answerIndex);
+            setQuizzPlay(true);
+        }
+    };
+
+    const getButtonColor = (answerIndex: number) => {
+        if (quizzPlay) {
+            if (answerIndex === data.answerTrue) {
+                return styles.green;
+            }
+            if (answerIndex !== data.answerTrue && answerIndex === answerChoisie) {
+                return styles.red;
+            }
+        }
+        return '';
+    };
+
+    return (
+        <div className={styles.container}>
+            <label className={styles.titre}>Quizz de sensibilisation</label> <br />
+            <label className={styles.label}>{data.question}</label> <br />
+            <button className={`${styles.button} ${getButtonColor(1)}`} onClick={() => handleResult(1)} disabled={quizzPlay}>{data.answer1}</button> <br />
+            <button className={`${styles.button} ${getButtonColor(2)}`} onClick={() => handleResult(2)} disabled={quizzPlay}>{data.answer2}</button> <br />
+            <button className={`${styles.button} ${getButtonColor(3)}`} onClick={() => handleResult(3)} disabled={quizzPlay}>{data.answer3}</button> <br />
+            {resultMessage && <p className={styles.message}>{resultMessage}</p>}
+            {(tempsRestant > 0 &&
+                <div>
+                    <p>Temps restant: {tempsRestant} secondes</p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Quizz;
