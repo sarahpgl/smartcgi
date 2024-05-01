f7558b50a7a7db45a145be281f0955a415a27e3d
ldelcourt <lou.delcourt@insa-lyon.fr>
2024-05-01 11:41:54 +0200

    Fix: Precedent commit

--- workspaces/api/src/game/game.gateway.ts
+++ workspaces/api/src/game/game.gateway.ts
@@ -19,70 +19,64 @@ import { LobbyCreateDto, LobbyJoinDto, PracticeAnswerDto } from '@app/game/dtos'
 import { WsValidationPipe } from '@app/websocket/ws.validation-pipe';
 
 @WebSocketGateway()
-export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
-{
+export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   private readonly logger: Logger = new Logger(GameGateway.name);
 
-  constructor(private readonly lobbyManager: LobbyManager) {}
+  constructor(private readonly lobbyManager: LobbyManager) { }
 
   afterInit(server: any) {
     this.lobbyManager.server = server;
   }
 
-  async handleConnection(client: Socket, ...args: any[]): Promise<void>
-  {
+  async handleConnection(client: Socket, ...args: any[]): Promise<void> {
     // Call initializers to set up socket
     const authenticatedClient: AuthenticatedSocket = client as AuthenticatedSocket;
     authenticatedClient.gameData = {
       lobby: null,
-      playerName: '',
+      playerName: null,
+      clientInGameId: null,
     }
 
     this.lobbyManager.initializeSocket(authenticatedClient);
   }
 
-  async handleDisconnect(client: AuthenticatedSocket): Promise<void>
-  {
+  async handleDisconnect(client: AuthenticatedSocket): Promise<void> {
     // Handle termination of socket
     this.lobbyManager.terminateSocket(client);
   }
 
   @SubscribeMessage(ClientEvents.Ping)
-  onPing(client: AuthenticatedSocket): void
-  {
+  onPing(client: AuthenticatedSocket): void {
     this.logger.log('Ping received ', client.id);
     client.emit(ServerEvents.Pong, {
       message: 'pong',
     });
   }
 
-  @SubscribeMessage(ClientEvents.LobbyCreate) 
+  @SubscribeMessage(ClientEvents.LobbyCreate)
   onLobbyCreate(client: AuthenticatedSocket, data: LobbyCreateDto) {
     const lobby = this.lobbyManager.createLobby(data.co2Quantity);
-    lobby.addClient(client, data.playerName, true);
+    lobby.addClient(client, data.playerName, null, true);
   }
 
   @SubscribeMessage(ClientEvents.LobbyJoin)
   onLobbyJoin(client: AuthenticatedSocket, data: LobbyJoinDto) {
-    this.lobbyManager.joinLobby(data.connectionCode, data.playerName, client);
+    this.lobbyManager.joinLobby(client, data.connectionCode, data.playerName, data.clientInGameId);
   }
 
   @SubscribeMessage(ClientEvents.LobbyLeave)
-  onLobbyLeave(client: AuthenticatedSocket): void
-  {
+  onLobbyLeave(client: AuthenticatedSocket): void {
     client.gameData.lobby?.removeClient(client);
   }
 
   @SubscribeMessage(ClientEvents.LobbyStartGame)
-  onLobbyStartGame(client: AuthenticatedSocket): void
-  {
+  onLobbyStartGame(client: AuthenticatedSocket): void {
     this.lobbyManager.startGame(client);
   }
 
-  @SubscribeMessage(ClientEvents.PracticeQuestion)
-  onPracticeQuestion(client: AuthenticatedSocket, data: PracticeAnswerDto): void
-  {
-    if(!client.gameData.lobby) {
+  @SubscribeMessage(ClientEvents.AnswerPracticeQuestion)
+  onPracticeQuestion(client: AuthenticatedSocket, data: PracticeAnswerDto): void {
+    if (!client.gameData.lobby) {
       throw new ServerException(SocketExceptions.GameError, 'Not in lobby');
     }
     client.gameData.lobby.instance.answerPracticeQuestion(client.id, data.cardId, data.answer);
