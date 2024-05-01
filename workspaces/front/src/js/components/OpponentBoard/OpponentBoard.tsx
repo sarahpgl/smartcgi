9df3c266a639243c897c094cf90dbc6abbf7c0a7
Gregoire <64920340+greg315315@users.noreply.github.com>
2024-05-01 17:55:21 +0200

    [Feat] CardHistory pour les opponent

--- workspaces/front/src/js/components/OpponentBoard/OpponentBoard.tsx
+++ workspaces/front/src/js/components/OpponentBoard/OpponentBoard.tsx
@@ -10,15 +10,16 @@ import styles from './OpponentBoard.module.css';
 import userIcon from '../../../icons/user_icon.webp';
 import PlayerStatus from '../PlayerStatus/PlayerStatus';
 import { PlayerStateInterface, PublicPlayerState } from '@shared/common/Game';
+import BadPracticeCard from '../BadPracticeCard/BadPracticeCard';
 
 const OpponentBoard: React.FC = () => {
 
     const data = {
-        name : "Jean",
+        name: "Jean",
         kg: "900",
         points: "3",
-        expert : ["Developer", "ProductOwner", "Architect"],
-        MP : "Developer"
+        expert: ["Developer", "ProductOwner", "Architect"],
+        MP: "Developer"
     };
 
     let player: PlayerStateInterface = {
@@ -38,17 +39,27 @@ const OpponentBoard: React.FC = () => {
         <div className={styles.opponentBoard}>
             <div className={styles.nameContainer}> </div>
             <div className={styles.container}>
-            <img src={userIcon} alt="user icon" className={styles.userIcon} />
-            <label className={styles.labelname}>{data.name}</label>
+                <img src={userIcon} alt="user icon" className={styles.userIcon} />
+                <label className={styles.labelname}>{data.name}</label>
 
-            <div className={styles.container2}>
-                <PlayerStatus playerstate={player} />
+                <div className={styles.container2}>
+                    <PlayerStatus playerstate={player} />
                 </div>
-                
-                <BestPracticeCard /> {}
+                <div className={styles.opponentHistory}>
+                    <div className={styles.card}>
+                        <BestPracticeCard cardType="BestPractice" id="32" title="titre de la carte" contents="blabla blabla blabla blabla blabla blabla blabla blabla blabla " carbon_loss={50} />
+                    </div>
+                    <div className={styles.card}>
+                        <BadPracticeCard cardType="BadPractice" id="32" title="titre de la carte" contents="blabla blabla blabla blabla blabla blabla blabla blabla blabla " targetedPlayer="Pierre" />
+                    </div>
+                    <div className={styles.card}>
+                        <BestPracticeCard cardType="BestPractice" id="32" title="titre de la carte" contents="blabla blabla blabla blabla blabla blabla blabla blabla blabla " carbon_loss={50} />
+                    </div>
             </div>
+            </div>
+
         </div>
-        
+
     );
 };
 
